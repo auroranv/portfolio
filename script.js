7d4b0bf90fb45601c5bc0c40987441dcac5676a3
aurora9721 <55090088+aurora9721@users.noreply.github.com>
2022-01-20 14:18:19 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-let myLabels =
-document.querySelectorAll('.lbl-toggle');
-Array.from(myLabels).forEach(label => {
-  label.addEventListener('keydown', e => { if
-    (e.which === 32 || e.which === 13) {
-      e.preventDefault(); label.click(); }; }); });
+jQuery(function($) {
+  $(window).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+
+          $('header').css({
+          opacity: function() {
+              var elementHeight = $(this).height(),
+              opacity = ((elementHeight - scrollTop) / elementHeight);
+              return opacity;
+          }
+      });
+  });
+})
+
+
+// jQuery(function($) {
+// var divs = $('.name'),
+//     limit = 300;  /* scrolltop value when opacity should be 0 */
+//
+// $(window).on('scroll', function() {
+//    var st = $(this).scrollTop();
+//
+//    /* avoid unnecessary call to jQuery function */
+//    if (st <= limit) {
+//      divs.css({ 'opacity' : (1 - st/limit)});
+//    }
+// });
+// })
